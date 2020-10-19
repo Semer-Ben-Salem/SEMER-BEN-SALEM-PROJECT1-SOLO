2038a4755367b2c2ab6782f4bd0f2742bee20d69
Semer-Ben-Salem <semer_ben_salem@yahoo.fr>
2020-10-19 14:08:14 +0100

    ...

--- main.js
+++ main.js
@@ -10,10 +10,28 @@
 
 // }
 
+/* -------------------- */
 
-for (var i=0 ; i<10 ; i++){
-var div = document.createElement("div")
-var grid = document.querySelector(".grid")
+
+for (var i=0 ; i<100 ; i++){
+    var div = document.createElement("div")
+    var grid = document.querySelector(".grid")
+    div.textContent = (Math.floor(Math.random()*2))
+    div.classList.add('gridstyle')
+    isHidden = div.hidden;
 grid.appendChild(div)
 
+
 }
+// grid.setAttribute("id", "gd");
+
+
+// for (var i=0 ; i<100 ; i++){
+//      div = Math.floor(Math.random()*2)
+// }
+
+// const div1 = document.querySelector("div")
+// var align = div1.getAttribute("wfd-id");
+// console.log(align)
+// document.getElementsByClassName('grid').value='100'
+
