90ed006013b4bb732e6b718a51824e3fc33e3927
Semer-Ben-Salem <semer_ben_salem@yahoo.fr>
2020-10-19 20:20:47 +0100

    ...

--- main.js
+++ main.js
@@ -1,38 +1,11 @@
+const $grid = ('#grid') ;
 
-// for (var i=0 ; i<100 ; i++){
-//     var div = document.createElement("div")
-//     var grid = document.querySelector(".grid")
-//     div.textContent = (Math.floor(Math.random()*2))
-   
-//     div.classList.add('gridstyle')
-// grid.appendChild(div)
+function CreatGrid (rows,columns){
 
-
-// }
-// function myFunction() {
-//     var x = document.getElementsByClassName("gridstyle");
-//     if (x.style.display === "none") {
-//       x.style.display = "block";
-//     } else {
-//       x.style.display = "none";
-//     }
-//   } 
-
-document.addEventListener('DOMContentLoaded', () =>{
-
-const array = []
-for (var i=0 ; i<5 ; i++){
-    array.push(Math.floor(Math.random()*2))
-}
-
-const grid = document.querySelector('.grid')
-// creat the game board
-
-function creatBoard (){
-
-    for (let i=0 ; i<5 ; i++){
-
+    for (var i=0 ; i<rows ; i++){
+        const $rows = $('<div>').addClass('rows');
+        $grid.append($row);
     }
 }
 
-})
+CreatGrid (10,10)
