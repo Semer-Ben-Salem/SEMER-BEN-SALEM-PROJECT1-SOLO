b0615dc8bba60940d4ef9a1496b33e606847282b
Semer-Ben-Salem <semer_ben_salem@yahoo.fr>
2020-10-19 16:01:26 +0100

    ...

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 
-for (var i=0 ; i<100 ; i++){
-    var div = document.createElement("div")
-    var grid = document.querySelector(".grid")
-    div.textContent = (Math.floor(Math.random()*2))
+// for (var i=0 ; i<100 ; i++){
+//     var div = document.createElement("div")
+//     var grid = document.querySelector(".grid")
+//     div.textContent = (Math.floor(Math.random()*2))
    
-    div.classList.add('gridstyle')
-grid.appendChild(div)
+//     div.classList.add('gridstyle')
+// grid.appendChild(div)
 
 
-}
-function myFunction() {
-    var x = document.getElementsByClassName("gridstyle");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  } 
+// }
+// function myFunction() {
+//     var x = document.getElementsByClassName("gridstyle");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   } 
+
+document.addEventListener('DOMContentLoaded', () =>{
+
+const cardArray =
+
+
+
+
+})
