d40bc0696d95bc79242a844f8707953049216bf1
Semer-Ben-Salem <semer_ben_salem@yahoo.fr>
2020-10-19 11:06:18 +0100

    ......................................

---
+++ main.js
@@ -0,0 +1,12 @@
+function filling (h,v){
+    var tab=[];
+    for (var i=0 ; i<5 ; i++){
+        for (var j=0 ; j<5 ; j++){
+            tab.push(Math.floor(Math.random()*2))
+        }
+    }
+
+    check(tab[])
+
+}
+
