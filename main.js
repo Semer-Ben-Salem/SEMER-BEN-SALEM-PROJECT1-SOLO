86f28c3aea9d5ace3e47a00510a9181d9a5c89de
Semer-Ben-Salem <semer_ben_salem@yahoo.fr>
2020-10-19 22:27:24 +0100

    ...

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
 const $board = $('#board') ;
 
 function createBoard(rows, cols) {
+    $board.empty();
     for (var i = 0; i < rows; i++) {
       const $row = $('<div>').addClass('row');
       for (var j = 0; j < cols; j++) {
         const $col = $('<div>')
         .addClass('col hidden')
         if (Math.random() < 0.1){
-            $col.addClass('beez');
+            $col.addClass('mine');
         }
         $row.append($col);
       }
       $board.append($row);
     }
   }
-
-  createBoard (10,10)
+  
+  function restart (){
+      createBoard(10, 10)
+  }
 
   function gameOver(isWin) {
-    var message;
+    var message = null ;
     if (isWin) {
       message = 'YOU WON!';
     } else {
       message = 'YOU LOST!';
      }
       alert(message);
+      restart();
  }
   
 
   $board.on('click', '.col.hidden', function(){
     const $cell = $(this);
-    if ($cell.hasClass('beez')){
-        gamOver(false);
+    if ($cell.hasClass('mine')){
+        gameOver(false);
     }    
 })
+
+restart();
