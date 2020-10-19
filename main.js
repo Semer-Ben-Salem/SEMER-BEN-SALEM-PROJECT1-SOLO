0fb21fa0e238124ccc28d038ac9090a8ca959304
Semer-Ben-Salem <semer_ben_salem@yahoo.fr>
2020-10-19 11:58:01 +0100

    ..

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-function filling (h,v){
-    var tab=[];
-    for (var i=0 ; i<5 ; i++){
-        for (var j=0 ; j<5 ; j++){
-            tab.push(Math.floor(Math.random()*2))
-        }
-    }
+// function filling (){
+//     var tab=[];
+//     for (var i=0 ; i<5 ; i++){
+//         for (var j=0 ; j<5 ; j++){
+//             tab.push(Math.floor(Math.random()*2))
+//         }
+//     }
 
-    check(tab[])
+//     return tab
+
+// }
+
+
+for (var i=0 ; i<10 ; i++){
+var div = document.createElement("div")
+var grid = document.querySelector(".grid")
+grid.appendChild(div)
 
 }
-
